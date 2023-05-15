8d9d0e0792c41557940e69b1d2d6c1f92fe085db
yujinlee <now8567@hanmail.net>
2023-05-15 23:27:01 +0900

    header 내용 추가

--- src/main/java/com/example/secondservice/SecondServiceController.java
+++ src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,16 +1,28 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// http://localhost:8082/welcome # zuul
+// http://localhost:8082/second-service/welcome # spring cloud gateway
 @RestController
-@RequestMapping("/")
+//@RequestMapping("/")
+@RequestMapping("/second-service")
+@Slf4j
 public class SecondServiceController {
 
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the Second service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request")String header){
+        log.info(header);
+        return "Hello World in Second Service.";
     }
 }
